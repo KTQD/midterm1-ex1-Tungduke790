1c97fba9da1b56991b2bc60f485ed770199361d7
Tungduke790 <157783262+Tungduke790@users.noreply.github.com>
2024-03-14 07:41:08 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,12 @@
-public class Book {
+public abstract class Book extends Item {
+    String author;
+    int numberOfPage;
+    String genre;
+
+    public Book(String name, String description, String id, float price, String author, int numberOfPage, String genre) {
+        super(name, description, id, price);
+        this.author = author;
+        this.numberOfPage = numberOfPage;
+        this.genre = genre;
+    }
 }
