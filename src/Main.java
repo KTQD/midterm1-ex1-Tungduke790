9830dad13c236d0525d30e0ee3c9da392411e436
Tungduke790 <157783262+Tungduke790@users.noreply.github.com>
2024-03-14 07:56:36 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,32 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
-        mp3.showInfo();
+        Book book = new Book("Jungle book", "Book about a jungler", "b1", 380, "Steven Hawkings", 88, "Storytelling") {
+            @Override
+            public void showInfo() {
+                System.out.println(name);
+                System.out.println(getDescription());
+                System.out.println(id);
+                System.out.println(price);
+                System.out.println(author);
+                System.out.println(numberOfPage);
+                System.out.println(genre);
+            }
+        };
 
-        Book book;
+        MP3 mp3 = new MP3("Cambridge audio", "Listening audio for Cambridge", "audio1", 672, 120) {
+            @Override
+            public void showInfo() {
+                System.out.println(name);
+                System.out.println(getDescription());
+                System.out.println(id);
+                System.out.println(price);
+                System.out.println(duration);
+            }
+        };
+
         book.showInfo();
-
+        mp3.showInfo();
     }
+
 }
